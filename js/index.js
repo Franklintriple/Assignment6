e5fe641855bf5278f02acf288cc424c4118f630f
Franklin <chukwuemekafranklin56@gmail.com>
2024-02-28 19:58:16 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,50 @@
+// var swiper = new Swiper(".slide-container", {
+//     slidesPerView: 4,
+//     spaceBetween: 20,
+//     sliderPerGroup: 4,
+//     loop: true,
+//     centerSlide: "true",
+//     fade: "true",
+//     grabCursor: "true",
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//       dynamicBullets: true,
+//     },
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//     breakpoints: {
+//       0: {
+//         slidesPerView: 1,
+//       },
+//       520: {
+//         slidesPerView: 2,
+//       },
+//       768: {
+//         slidesPerView: 3,
+//       },
+//       1000: {
+//         slidesPerView: 4,
+//       },
+//     },
+//   });
+//   <script>
+//   var swiper = new Swiper(".mySwiper", {
+//     spaceBetween: 30,
+//     centeredSlides: true,
+//     autoplay: {
+//       delay: 2500,
+//       disableOnInteraction: false,
+//     },
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//   });
+// </script>
